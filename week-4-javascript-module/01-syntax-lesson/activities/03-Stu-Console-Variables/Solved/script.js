2cc1c8aaf1aa2daded287f5bec2af38bbca676a3
demuelle <dmueller@trilogyed.com>
2022-12-19 12:04:44 -0600

    Adds week 4 activity solutions

---
+++ week-4-javascript-module/01-syntax-lesson/activities/03-Stu-Console-Variables/Solved/script.js
@@ -0,0 +1,19 @@
+// Declare variables using var
+var personName = 'Dan';
+var pets = 1;
+var funFact = 'Smiling is my favorite.';
+
+// Use + to combine data and variable names in a single console log
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
+
+// When re-assigning variables, use variable name
+personName = 'Manny';
+pets = 0;
+funFact = 'I was born in 2019, but they tell me I am 21 years.';
+
+// Logs message with re-assigned values
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact);
