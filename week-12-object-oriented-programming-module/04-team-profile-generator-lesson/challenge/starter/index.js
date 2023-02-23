c812a150bd2654255311b88625c4dd28d14f5151
Lucas Rambourg <lucasrambourg11@gmail.com>
2023-02-23 22:50:45 +0000

    questions and functions added

--- week-12-object-oriented-programming-module/04-team-profile-generator-lesson/challenge/starter/index.js
+++ week-12-object-oriented-programming-module/04-team-profile-generator-lesson/challenge/starter/index.js
@@ -13,3 +13,89 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+// array of questions for user
+const questionsManager = [
+     
+    { message: "What is your name?",
+      name: 'managerName',
+   },
+
+   { message: "What is your ID number?",
+      name: 'managerId',
+   },
+
+   { message: "What is your office number?",
+      name: 'managerNumber',
+
+
+   },
+
+   { message: `Would you like to add an engineer details press 1, 
+   press 2 to add an intern
+   press 3 to finish building the team?`,
+      name: 'options',
+
+
+   },
+
+];
+
+
+const questionsEngineer =[
+
+    { message: "What is your name?",
+    name: 'engineerName',
+ },
+
+ { message: "What is your ID?",
+ name: 'engineerId',
+},
+
+{ message: "What is your email?",
+name: 'engineerEmail',
+},
+
+{ message: "What is your Github UserName?",
+name: 'engineerGithub',
+},
+
+];
+
+const questionsIntern =[
+
+    { message: "What is your name?",
+    name: 'internName',
+ },
+
+ { message: "What is your ID?",
+ name: 'internId',
+},
+
+{ message: "What is your email?",
+name: 'internEmail',
+},
+
+{ message: "What is your school?",
+name: 'internSchool',
+},
+
+]
+
+
+// function call to initialize program
+async function init() {
+    inquirer.prompt(questionsManager)
+  .then((inquirerResponse) => {   
+      
+  })
+  .catch((err) => {
+      console.log(err);
+  })
+   
+  };
+ 
+
+
+init();
+
