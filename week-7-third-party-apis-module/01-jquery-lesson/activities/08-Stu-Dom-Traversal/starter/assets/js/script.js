3d5692c1f37142bdc14a703b44112874cb57208f
Lucas Rambourg <116593146+Rambourg@users.noreply.github.com>
2023-02-24 00:09:56 +0000

    Delete week-7-third-party-apis-module directory

--- week-7-third-party-apis-module/01-jquery-lesson/activities/08-Stu-Dom-Traversal/starter/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-// Assemble: Create/select DOM elements
-var rootEl = $('#root');
-
-// TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
-$("li").css("background-color", "white");
-
-// TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
-$("ul").eq(1).children().eq(2).text("y")
