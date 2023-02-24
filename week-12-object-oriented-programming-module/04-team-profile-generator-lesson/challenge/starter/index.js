72846a8b6a8aab29fdb0b247027da04e8213b345
Lucas Rambourg <lucasrambourg11@gmail.com>
2023-02-24 22:47:09 +0000

    added questions and functions for each employee

--- week-12-object-oriented-programming-module/04-team-profile-generator-lesson/challenge/starter/index.js
+++ week-12-object-oriented-programming-module/04-team-profile-generator-lesson/challenge/starter/index.js
@@ -1,3 +1,6 @@
+// import { start } from "repl";
+// import Employee from "./lib/Employee";
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -8,10 +11,25 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+const render = require("./src/page-template.js")
+;
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+// let team = [];
+// startProgram(); {
+//    team.push (new employee("ferf"));
+//    team.push (new engineer("ferfrrf"));
+//    team.push (new intern(""));
+//    team.push ( new Manager(""));
+
+//    let htmlDoc = render(team);
+
+//    await fs.writeFile(outputPath, htmlDoc);
+// };
+
+
 
 
 // array of questions for user
@@ -20,6 +38,11 @@ const questionsManager = [
     { message: "What is your name?",
       name: 'managerName',
    },
+
+   { message: "What is your email address?",
+   name: 'managerEmail',
+},
+
 
    { message: "What is your ID number?",
       name: 'managerId',
@@ -31,16 +54,14 @@ const questionsManager = [
 
    },
 
-   { message: `Would you like to add an engineer details press 1, 
-   press 2 to add an intern
-   press 3 to finish building the team?`,
-      name: 'options',
-
-
-   },
-
 ];
 
+const generalQuestion = [ { message: `Would you like to add an engineer details press 1, 
+press 2 to add an intern
+press 3 to finish building the team?`,
+   name: 'options',
+},
+] 
 
 const questionsEngineer =[
 
@@ -83,11 +104,59 @@ name: 'internSchool',
 ]
 
 
+// promptUser()
+//   .then((team) => writeFileAsync('index.html', generateHTML(team)))
+//   .then(() => console.log('Successfully wrote to index.html'))
+//   .catch((err) => console.error(err));
+ 
+
+
 // function call to initialize program
 async function init() {
     inquirer.prompt(questionsManager)
-  .then((inquirerResponse) => {   
+  .then((inquirerResponse) => { 
+
+   console.log(inquirerResponse);
+   
+   const managerObj = new Manager(inquirerResponse.managerName,inquirerResponse.managerId,inquirerResponse.managerEmail, inquirerResponse.managerNumber);
+  
+   // while (true) { 
+      inquirer.prompt (generalQuestion) 
+
+      .then ((inquirerOptions)=>{ 
+         const option = inquirerOptions.options;
+
       
+         console.log(option);
+
+         inquirer.prompt ( questionsEngineer)
+
+
+         .then ((inquirerOptions)=>{ 
+            const option = inquirerOptions.options;
+   
+            console.log(option);
+   
+          
+         })
+         inquirer.prompt (questionsIntern) 
+
+         .then ((inquirerOptions)=>{ 
+            const option = inquirerOptions.options;
+   
+            console.log(option);
+   
+         
+   
+         })
+
+         
+       
+
+      })
+   
+   // }
+ 
   })
   .catch((err) => {
       console.log(err);
